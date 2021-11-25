e11ca8b02e918c2e05d9970106e2a8fbe61a3ac4
kevin2-cyber <73193206+kevin2-cyber@users.noreply.github.com>
2021-11-24 18:59:48 -0800

    python

--- first/main.py
+++ first/main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+class Car:
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def __init__(self, seats, tyres):
+        self.seats = seats
+        self.tyres = tyres
 
+    def drive(self):
+        return self.seats
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
