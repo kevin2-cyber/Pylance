b9cf81ecb13550860a36f0241d8d09ee2498c6f9
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-30 16:14:42 +0000

    functions and generators

--- first/main.py
+++ first/main.py
@@ -1,8 +1,35 @@
-class Car:
 
-    def __init__(self, seats, tyres):
-        self.seats = seats
-        self.tyres = tyres
+messages = 'Hello World'
+# Integers
+# Floats
+# constants
+print(messages)
+age = 10
+age_new = [10, 20,30]
+print(type(age_new))
+person_details = {
+    'name': 'Kofi',
+    'Programme': 'Computer Science',
+    'Level': 200,
+}
 
-    def drive(self):
-        return self.seats
+
+age = 17 
+
+if age < 18:
+    print("You're mot eligible to vote")
+else:
+    print("You're eligible to vote")
+# functions
+def  test(*args):
+    print(args)
+    
+
+# genrators
+def gen(*args):
+    for arg in args:
+        yield arg
+        
+val = gen(1,2,3,4,5,6)
+for v in val:
+    print(v)
