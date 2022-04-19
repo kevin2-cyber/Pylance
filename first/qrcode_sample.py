d4e25a4824989671626aa2383dda122a5af4ffc2
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-19 21:41:52 +0000

    qrcode

--- first/qrcode_sample.py
+++ first/qrcode_sample.py
@@ -5,3 +5,15 @@ img.save('hello.png')
 
 testing = qrcode.make('hi world')
 testing.save('hi.png')
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data("https://abhijithchandradas.medium.com/")
+qr.make(fit=True)
+
+image = qr.make_image(fill_color="red", back_color="black")
+image.save("medium.png")
