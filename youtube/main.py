39024a358a7c05f41da4fd31ad41a1cf8ea04a86
kevin2-cyber <keduful@st.ug.edu.gh>
2022-05-16 00:34:30 +0000

    didn't really workout as planned

--- youtube/main.py
+++ youtube/main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+from pytube import YouTube
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+video_link = "https://youtu.be/YY-_yrZdjGc"
 
+video = YouTube(video_link)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+video.streams.filter(res='1080p').first().download('C:\\Downloads')
