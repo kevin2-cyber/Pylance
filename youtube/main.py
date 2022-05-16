a6aff9023ad391b3f187f3bb7427355910fc26ff
kevin2-cyber <keduful@st.ug.edu.gh>
2022-05-16 00:43:58 +0000

    new

--- youtube/main.py
+++ youtube/main.py
@@ -4,4 +4,5 @@ video_link = "https://youtu.be/YY-_yrZdjGc"
 
 video = YouTube(video_link)
 
-video.streams.filter(res='1080p').first().download('C:\\Downloads')
+downloads = 'C:\\Downloads'
+video.streams.filter(res='1080p').first().download(downloads)
