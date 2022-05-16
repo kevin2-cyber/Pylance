75bc0a0b950e81bf5e78781b72b6efeb47aac413
kevin2-cyber <keduful@st.ug.edu.gh>
2022-05-16 01:02:22 +0000

    finally it worked

--- youtube/main.py
+++ youtube/main.py
@@ -1,8 +1,33 @@
 from pytube import YouTube
 
+downloads = 'C:\\Downloads'
+
 video_link = "https://youtu.be/YY-_yrZdjGc"
 
-video = YouTube(video_link)
+try:
+    video = YouTube(video_link)
+except:
+    print("video isn't downloading")
 
-downloads = 'C:\\Downloads'
-video.streams.filter(res='1080p').first().download(downloads)
+print(video.title)
+print(video.views)
+print(video.author)
+print(video.description)
+
+lst = video.streams.filter()
+for x in lst:
+    print(x)
+
+print(video.streams.filter(progressive=True).get_highest_resolution().itag)
+video_itag = video.streams.filter(progressive=True).get_highest_resolution().itag
+
+try:
+    video.streams.get_by_itag(video_itag).download(output_path=downloads)
+except:
+    print("Error downloading")
+
+print("Video downloaded")
+
+
+
+
