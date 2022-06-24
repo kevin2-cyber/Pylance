8c0666982f7b1eb3dbc827548bb793a0c096a4f8
kevin2-cyber <keduful@st.ug.edu.gh>
2022-06-24 13:37:06 +0000

    Reliability Engineering

--- youtube/main.py
+++ youtube/main.py
@@ -2,7 +2,7 @@ from pytube import YouTube, StreamQuery
 
 downloads: str = 'C:\\Downloads'
 
-video_link: str = "https://youtu.be/mGHuHQDNetA"
+video_link: str = "https://youtu.be/8Dn2UQ__8QM"
 
 try:
     video: YouTube = YouTube(video_link)
