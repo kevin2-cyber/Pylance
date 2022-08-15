70624fa4f5849ea60ff031a1ef282f07efb79b72
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-15 19:37:23 +0000

    new

--- youtube/play_list.py
+++ youtube/play_list.py
@@ -1,1 +1,14 @@
-var = __name__ == "__main__"
+num = input("Enter number")
+num = int(num)
+sum = 0
+average = 0
+for i in range (2,num+1):
+    for j in range(2,i):
+        if(i%j==0):
+            break
+    else:
+        print(i)
+        sum=sum+i
+        average = sum/i
+print("Sum of prime number is",sum)
+print("average is",average)
