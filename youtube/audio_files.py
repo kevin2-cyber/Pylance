8119c209e652321bb7056f41798d45625d6df18c
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-29 16:01:17 +0000

    new

--- youtube/audio_files.py
+++ youtube/audio_files.py
@@ -2,7 +2,7 @@ from pytube import YouTube, StreamQuery
 
 downloads: str = 'C:\\Downloads'
 
-audio_link: str = "https://youtu.be/13PYVofBFRc"
+audio_link: str = "https://youtu.be/ivUb1K0B0zE"
 
 try:
     audio: YouTube = YouTube(audio_link)
@@ -22,7 +22,7 @@ print(audio.streams.filter(only_audio=True).get_audio_only().itag)
 audio_itag: int = audio.streams.filter(only_audio=True).get_audio_only().itag
 
 try:
-    file_name: str = "Kingdom.mp3"
+    file_name: str = "Yeshua.mp3"
     audio.streams.get_by_itag(audio_itag).download(output_path=downloads, filename=file_name)
 finally:
     print("Error downloading")
