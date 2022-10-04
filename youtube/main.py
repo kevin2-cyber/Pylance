f5a5932dab798f521e3381f9e9a03896bb06f46a
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-10-04 11:22:22 +0000

    java

--- youtube/main.py
+++ youtube/main.py
@@ -2,7 +2,7 @@ from pytube import YouTube, StreamQuery
 
 downloads: str = 'C:\\Downloads'
 
-video_link: str = "https://youtu.be/oiraX_YypPI?list=RDMM"
+video_link: str = "https://youtu.be/A74TOX803D0"
 
 try:
     video: YouTube = YouTube(video_link)
