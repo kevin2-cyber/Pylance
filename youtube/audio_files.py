93fb7442c3ff64c1848f750811cd41f05c71b778
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-10-26 02:06:30 +0000

    Ancient gates

--- youtube/audio_files.py
+++ youtube/audio_files.py
@@ -2,7 +2,7 @@ from pytube import YouTube, StreamQuery
 
 downloads: str = 'C:\\Downloads'
 
-audio_link: str = "https://youtu.be/xtKNlzYdoBM?list=PLIufuwOIYxaHMol1LVuy1cb3p9v4JOETJ"
+audio_link: str = "https://youtu.be/xw38lNKZQ_I?list=PLFX5WHd89PSrV3c196Am786S13x6XQCqL"
 
 try:
     audio: YouTube = YouTube(audio_link)
@@ -24,7 +24,7 @@ print(audio.streams.filter(only_audio=True).get_audio_only().itag)
 audio_itag: int = audio.streams.filter(only_audio=True).get_audio_only().itag
 
 try:
-    file_name: str = "Darlene Zschech Emmanuel.mp3"
+    file_name: str = "Brooke Ligertwood Ancient Gates (Live).mp3"
     audio.streams.get_by_itag(audio_itag).download(output_path=downloads, filename=file_name)
 except:
     print("Error downloading")
