8c9f7b294902e2b52f4ebf04306a39c884ed3863
kevin2-cyber <keduful@st.ug.edu.gh>
2023-02-14 19:56:59 +0000

    I wiil be calling you

--- youtube/audio_files.py
+++ youtube/audio_files.py
@@ -2,7 +2,7 @@ from pytube import YouTube, StreamQuery
 
 downloads: str = '/Users/kelvineduful/Downloads/youtube'
 
-audio_link: str = "https://youtu.be/V0vfz5lr37g"
+audio_link: str = "https://youtu.be/dogq83Y_3tA"
 
 try:
     audio: YouTube = YouTube(audio_link)
@@ -24,7 +24,7 @@ print(audio.streams.filter(only_audio=True).get_audio_only().itag)
 audio_itag: int = audio.streams.filter(only_audio=True).get_audio_only().itag
 
 try:
-    file_name: str = "All I Have Is Christ [Sovereign Grace Music].mp3"
+    file_name: str = "First Love Music - I Will Be Calling You.mp3"
     audio.streams.get_by_itag(audio_itag).download(output_path=downloads, filename=file_name)
 except:
     print("Error downloading")
