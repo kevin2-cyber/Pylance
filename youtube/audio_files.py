fd42c60ab6ac41c9429981b17539e1bb6209d120
kevin2-cyber <keduful@st.ug.edu.gh>
2023-03-01 01:55:05 +0000

    I Desire Jesus

--- youtube/audio_files.py
+++ youtube/audio_files.py
@@ -2,7 +2,7 @@ from pytube import YouTube, StreamQuery
 
 downloads: str = '/Users/kelvineduful/Downloads/youtube'
 
-audio_link: str = "https://youtu.be/oiraX_YypPI"
+audio_link: str = "https://youtu.be/KM07zyBP39M"
 
 try:
     audio: YouTube = YouTube(audio_link)
@@ -24,7 +24,7 @@ print(audio.streams.filter(only_audio=True).get_audio_only().itag)
 audio_itag: int = audio.streams.filter(only_audio=True).get_audio_only().itag
 
 try:
-    file_name: str = "Saviour - Hillsong Worship.mp3"
+    file_name: str = "I Desire Jesus - Hillsong Worship.mp3"
     audio.streams.get_by_itag(audio_itag).download(output_path=downloads, filename=file_name)
 except:
     print("Error downloading")
