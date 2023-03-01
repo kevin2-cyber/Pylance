4b60bb9b9a08ab78f2e7965db9044e1126e88fc4
kevin2-cyber <keduful@st.ug.edu.gh>
2023-03-01 14:55:31 +0000

    Echoes

--- youtube/audio_files.py
+++ youtube/audio_files.py
@@ -2,7 +2,7 @@ from pytube import YouTube, StreamQuery
 
 downloads: str = '/Users/kelvineduful/Downloads/youtube'
 
-audio_link: str = "https://youtu.be/rDXTqTuG1Nk"
+audio_link: str = "https://youtu.be/rFBuh72C2zo"
 
 try:
     audio: YouTube = YouTube(audio_link)
@@ -24,7 +24,7 @@ print(audio.streams.filter(only_audio=True).get_audio_only().itag)
 audio_itag: int = audio.streams.filter(only_audio=True).get_audio_only().itag
 
 try:
-    file_name: str = "Darlene Zschech - My Jesus, I Love Thee | Official Live Video.mp3"
+    file_name: str = "Echoes (Till We See The Other Side) [Live] Hillsong UNITED.mp3"
     audio.streams.get_by_itag(audio_itag).download(output_path=downloads, filename=file_name)
 except:
     print("Error downloading")
