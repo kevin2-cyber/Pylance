295b8002e1032f70138097d5cde10282179984c6
kevin2-cyber <keduful@st.ug.edu.gh>
2023-03-02 16:48:12 +0000

    new

--- youtube/main.py
+++ youtube/main.py
@@ -2,7 +2,7 @@ from pytube import YouTube, StreamQuery
 
 downloads: str = '/Users/kelvineduful/Downloads/youtube'
 
-video_link: str = "https://youtube.com/shorts/SxUr_ldKC_g?feature=share"
+video_link: str = "https://youtu.be/xUodSGUkwGM"
 
 try:
     video: YouTube = YouTube(video_link)
