9d2668268f3d966fa3dc8ea8624f30841d19d19e
kevin2-cyber <keduful@st.ug.edu.gh>
2023-03-06 03:37:22 +0000

    Bones

--- youtube/audio_files.py
+++ youtube/audio_files.py
@@ -2,7 +2,7 @@ from pytube import YouTube, StreamQuery
 
 downloads: str = '/Users/kelvineduful/Downloads/youtube'
 
-audio_link: str = "https://youtu.be/rFBuh72C2zo"
+audio_link: str = "https://youtu.be/xp5YNx9Xm8Y"
 
 try:
     audio: YouTube = YouTube(audio_link)
@@ -24,7 +24,7 @@ print(audio.streams.filter(only_audio=True).get_audio_only().itag)
 audio_itag: int = audio.streams.filter(only_audio=True).get_audio_only().itag
 
 try:
-    file_name: str = "Echoes (Till We See The Other Side) [Live] Hillsong UNITED.mp3"
+    file_name: str = "BONES - LIVE IN MIAMI - Hillsong UNITED.mp3"
     audio.streams.get_by_itag(audio_itag).download(output_path=downloads, filename=file_name)
 except:
     print("Error downloading")
