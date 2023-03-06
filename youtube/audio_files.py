3877e81970a3eff9cb65e95b1645717bc673daf2
kevin2-cyber <keduful@st.ug.edu.gh>
2023-03-06 03:49:31 +0000

    His Glory Appears

--- youtube/audio_files.py
+++ youtube/audio_files.py
@@ -2,7 +2,7 @@ from pytube import YouTube, StreamQuery
 
 downloads: str = '/Users/kelvineduful/Downloads/youtube'
 
-audio_link: str = "https://youtu.be/bDbvKqXLL7k"
+audio_link: str = "https://youtu.be/UJRR3TEjm7Q"
 
 try:
     audio: YouTube = YouTube(audio_link)
@@ -24,7 +24,7 @@ print(audio.streams.filter(only_audio=True).get_audio_only().itag)
 audio_itag: int = audio.streams.filter(only_audio=True).get_audio_only().itag
 
 try:
-    file_name: str = "Lord Of Lords - Hillsong Worship.mp3"
+    file_name: str = "His Glory Appears - Hillsong Worship.mp3"
     audio.streams.get_by_itag(audio_itag).download(output_path=downloads, filename=file_name)
 except:
     print("Error downloading")
