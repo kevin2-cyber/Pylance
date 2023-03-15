b39145fed2724075cb3b9afa1d03e8dde11eff45
kevin2-cyber <keduful@st.ug.edu.gh>
2023-03-15 20:48:05 +0000

    God is here

--- youtube/audio_files.py
+++ youtube/audio_files.py
@@ -2,7 +2,7 @@ from pytube import YouTube, StreamQuery
 
 downloads: str = '/Users/kelvineduful/Downloads/youtube'
 
-audio_link: str = "https://youtu.be/UZU93eCHhHA"
+audio_link: str = "https://youtu.be/nedEQtfQIvA"
 
 try:
     audio: YouTube = YouTube(audio_link)
@@ -24,7 +24,7 @@ print(audio.streams.filter(only_audio=True).get_audio_only().itag)
 audio_itag: int = audio.streams.filter(only_audio=True).get_audio_only().itag
 
 try:
-    file_name: str = "Darlene Zschech - Best For Me (Official Audio).mp3"
+    file_name: str = "Darlene Zschech - God Is Here (Live).mp3"
     audio.streams.get_by_itag(audio_itag).download(output_path=downloads, filename=file_name)
 except:
     print("Error downloading")
