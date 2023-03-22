51468e81a02408a21f393cf7dde7ba6fa0b99162
kevin2-cyber <keduful@st.ug.edu.gh>
2023-03-22 18:31:59 +0000

    From the inside out

--- youtube/audio_files.py
+++ youtube/audio_files.py
@@ -2,7 +2,7 @@ from pytube import YouTube, StreamQuery
 
 downloads: str = '/Users/kelvineduful/Downloads/youtube'
 
-audio_link: str = "https://youtu.be/uJVi0j6Zr4k"
+audio_link: str = "https://youtu.be/ZlLSKnJmCmY"
 
 try:
     audio: YouTube = YouTube(audio_link)
@@ -24,7 +24,7 @@ print(audio.streams.filter(only_audio=True).get_audio_only().itag)
 audio_itag: int = audio.streams.filter(only_audio=True).get_audio_only().itag
 
 try:
-    file_name: str = "You'll Come - Hillsong Worship.mp3"
+    file_name: str = "FROM THE INSIDE OUT - LIVE IN MIAMI - Hillsong UNITED.mp3"
     audio.streams.get_by_itag(audio_itag).download(output_path=downloads, filename=file_name)
 except:
     print("Error downloading")
