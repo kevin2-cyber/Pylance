86686e9dacbc729e1979f4031ef15c94f9d5e5f8
kevin2-cyber <keduful@st.ug.edu.gh>
2023-03-22 19:15:07 +0000

    God's not dead

--- youtube/audio_files.py
+++ youtube/audio_files.py
@@ -2,7 +2,7 @@ from pytube import YouTube, StreamQuery
 
 downloads: str = '/Users/kelvineduful/Downloads/youtube'
 
-audio_link: str = "https://youtu.be/N2h9Hm9JZho"
+audio_link: str = "https://youtu.be/S_OTz-lpDjw"
 
 try:
     audio: YouTube = YouTube(audio_link)
@@ -24,7 +24,7 @@ print(audio.streams.filter(only_audio=True).get_audio_only().itag)
 audio_itag: int = audio.streams.filter(only_audio=True).get_audio_only().itag
 
 try:
-    file_name: str = "Passion - Let It Be Jesus (Live) ft. Christy Nockels.mp3"
+    file_name: str = "Newsboys - God's Not Dead.mp3"
     audio.streams.get_by_itag(audio_itag).download(output_path=downloads, filename=file_name)
 except:
     print("Error downloading")
