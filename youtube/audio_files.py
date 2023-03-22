ff101e408ffd80a29203bd634989ef3828b81de8
kevin2-cyber <keduful@st.ug.edu.gh>
2023-03-22 14:40:46 +0000

    Good good father

--- youtube/audio_files.py
+++ youtube/audio_files.py
@@ -2,7 +2,7 @@ from pytube import YouTube, StreamQuery
 
 downloads: str = '/Users/kelvineduful/Downloads/youtube'
 
-audio_link: str = "https://youtu.be/lKw6uqtGFfo"
+audio_link: str = "https://youtu.be/qlsQrycKKsY"
 
 try:
     audio: YouTube = YouTube(audio_link)
@@ -24,7 +24,7 @@ print(audio.streams.filter(only_audio=True).get_audio_only().itag)
 audio_itag: int = audio.streams.filter(only_audio=True).get_audio_only().itag
 
 try:
-    file_name: str = "Who You Say I Am - Hillsong Worship.mp3"
+    file_name: str = "Chris Tomlin - Good Good Father ft. Pat Barrett.mp3"
     audio.streams.get_by_itag(audio_itag).download(output_path=downloads, filename=file_name)
 except:
     print("Error downloading")
