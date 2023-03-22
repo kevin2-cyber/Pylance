52d3b51e18d00a8f00a076c21a4372255b9378f3
kevin2-cyber <keduful@st.ug.edu.gh>
2023-03-22 19:08:35 +0000

    The Stand

--- youtube/audio_files.py
+++ youtube/audio_files.py
@@ -2,7 +2,7 @@ from pytube import YouTube, StreamQuery
 
 downloads: str = '/Users/kelvineduful/Downloads/youtube'
 
-audio_link: str = "https://youtu.be/tk5yLJbQCbI"
+audio_link: str = "https://youtu.be/EruXNSaSJAc"
 
 try:
     audio: YouTube = YouTube(audio_link)
@@ -24,7 +24,7 @@ print(audio.streams.filter(only_audio=True).get_audio_only().itag)
 audio_itag: int = audio.streams.filter(only_audio=True).get_audio_only().itag
 
 try:
-    file_name: str = "Shout to The Lord & Agnus Dei (Darlene Zschech - Revealing Jesus).mp3"
+    file_name: str = "THE STAND - Hillsong UNITED - LIVE IN MIAMI.mp3"
     audio.streams.get_by_itag(audio_itag).download(output_path=downloads, filename=file_name)
 except:
     print("Error downloading")
