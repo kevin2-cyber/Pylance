8a3e43d578cc87713c4bd326c2c752cdd85b945d
kevin2-cyber <keduful@st.ug.edu.gh>
2023-03-22 18:07:54 +0000

    How he loves us

--- youtube/audio_files.py
+++ youtube/audio_files.py
@@ -2,7 +2,7 @@ from pytube import YouTube, StreamQuery
 
 downloads: str = '/Users/kelvineduful/Downloads/youtube'
 
-audio_link: str = "https://youtu.be/qlsQrycKKsY"
+audio_link: str = "https://youtu.be/A5t7Uoanryg"
 
 try:
     audio: YouTube = YouTube(audio_link)
@@ -24,7 +24,7 @@ print(audio.streams.filter(only_audio=True).get_audio_only().itag)
 audio_itag: int = audio.streams.filter(only_audio=True).get_audio_only().itag
 
 try:
-    file_name: str = "Chris Tomlin - Good Good Father ft. Pat Barrett.mp3"
+    file_name: str = "Passion - How He Loves (Live) ft. Crowder.mp3"
     audio.streams.get_by_itag(audio_itag).download(output_path=downloads, filename=file_name)
 except:
     print("Error downloading")
