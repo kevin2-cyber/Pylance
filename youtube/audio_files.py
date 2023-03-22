c92032734e383048b1b2254fa339050a34d621a4
kevin2-cyber <keduful@st.ug.edu.gh>
2023-03-22 18:39:54 +0000

    can't download

--- youtube/audio_files.py
+++ youtube/audio_files.py
@@ -2,7 +2,7 @@ from pytube import YouTube, StreamQuery
 
 downloads: str = '/Users/kelvineduful/Downloads/youtube'
 
-audio_link: str = "https://youtu.be/FEVVl9wCovQ"
+audio_link: str = "https://youtu.be/qceQsAAyah0"
 
 try:
     audio: YouTube = YouTube(audio_link)
@@ -24,7 +24,7 @@ print(audio.streams.filter(only_audio=True).get_audio_only().itag)
 audio_itag: int = audio.streams.filter(only_audio=True).get_audio_only().itag
 
 try:
-    file_name: str = "Place Of Worship - William McDowell.mp3"
+    file_name: str = "William McDowell- Closer/Wrap Me In Your Arms feat. Blanca.mp3"
     audio.streams.get_by_itag(audio_itag).download(output_path=downloads, filename=file_name)
 except:
     print("Error downloading")
