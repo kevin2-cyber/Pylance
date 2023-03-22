2af0dabe257e636fc1b03524d2090f81f1dd5a73
kevin2-cyber <keduful@st.ug.edu.gh>
2023-03-22 18:58:35 +0000

    O the blood

--- youtube/audio_files.py
+++ youtube/audio_files.py
@@ -2,7 +2,7 @@ from pytube import YouTube, StreamQuery
 
 downloads: str = '/Users/kelvineduful/Downloads/youtube'
 
-audio_link: str = "https://youtu.be/bfveawSAHJA?list=PLA3ug0-cTBrNHeC54wWOPDE9381UMMXY9"
+audio_link: str = "https://youtu.be/Z2Ydp6iWlGg"
 
 try:
     audio: YouTube = YouTube(audio_link)
@@ -24,7 +24,7 @@ print(audio.streams.filter(only_audio=True).get_audio_only().itag)
 audio_itag: int = audio.streams.filter(only_audio=True).get_audio_only().itag
 
 try:
-    file_name: str = "Kari Jobe - I Am Not Alone (Live).mp3"
+    file_name: str = "Gateway - O The Blood.mp3"
     audio.streams.get_by_itag(audio_itag).download(output_path=downloads, filename=file_name)
 except:
     print("Error downloading")
