8f4c1eb3ebb7e5ad3052120104b3bdefbd04b203
kevin2-cyber <keduful@st.ug.edu.gh>
2023-03-24 14:52:47 +0000

    Developer roadmap for android

--- youtube/main.py
+++ youtube/main.py
@@ -2,7 +2,7 @@ from pytube import YouTube, StreamQuery
 
 downloads: str = '/Users/kelvineduful/Downloads/youtube'
 
-video_link: str = "https://youtu.be/xUodSGUkwGM"
+video_link: str = "https://youtu.be/AhUL5tHF3uc"
 
 try:
     video: YouTube = YouTube(video_link)
