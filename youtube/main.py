0eccd223fbff0dd812ff2f7baa1d67abb7fd341d
kevin2-cyber <keduful@st.ug.edu.gh>
2023-04-18 21:10:39 +0000

    See how far

--- youtube/main.py
+++ youtube/main.py
@@ -2,7 +2,7 @@ from pytube import YouTube, StreamQuery
 
 downloads: str = '/Users/kelvineduful/Downloads/youtube'
 
-video_link: str = "https://youtu.be/XZwOuPUwyuk"
+video_link: str = "https://youtu.be/4MCwKdai51o"
 
 try:
     video: YouTube = YouTube(video_link)
