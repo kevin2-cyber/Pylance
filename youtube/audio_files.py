7650f4d38dd8948c15b2c382cf04a91731e19bff
kevin2-cyber <keduful@st.ug.edu.gh>
2023-04-18 21:12:33 +0000

    He won't

--- youtube/audio_files.py
+++ youtube/audio_files.py
@@ -2,7 +2,7 @@ from pytube import YouTube, StreamQuery
 
 downloads: str = '/Users/kelvineduful/Downloads/youtube'
 
-audio_link: str = "https://youtu.be/FHtesV8Do8c"
+audio_link: str = "https://youtu.be/uOP4s8fOEm0"
 
 try:
     audio: YouTube = YouTube(audio_link)
@@ -24,7 +24,7 @@ print(audio.streams.filter(only_audio=True).get_audio_only().itag)
 audio_itag: int = audio.streams.filter(only_audio=True).get_audio_only().itag
 
 try:
-    file_name: str = "SEE HOW FAR - VICTORIA ORENZE FT. NATHANIEL BASSEY, DUNSIN OYEKAN.mp3"
+    file_name: str = "Firm Foundation (He Won’t) [feat. Chandler Moore & Cody Carnes] | Maverick City Music | TRIBL.mp3"
     audio.streams.get_by_itag(audio_itag).download(output_path=downloads, filename=file_name)
 except:
     print("Error downloading")
