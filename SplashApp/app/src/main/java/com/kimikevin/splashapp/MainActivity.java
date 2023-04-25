ebbad2e7fa5ab008f3e11c7e669f171c29604f86
kevin2-cyber <keduful@st.ug.edu.gh>
2023-04-25 04:12:58 +0000

    trying on splash screen

---
+++ SplashApp/app/src/main/java/com/kimikevin/splashapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.kimikevin.splashapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kimikevin.splashapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getSplashScreen().setSplashScreenTheme(R.style.SplashTheme);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        super.onCreate(savedInstanceState);
+        setContentView(binding.getRoot());
+    }
+}
