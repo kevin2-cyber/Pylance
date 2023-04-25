6fcc35e576211e952ee2b64800ccf33917ddf8ba
kevin2-cyber <keduful@st.ug.edu.gh>
2023-04-25 16:31:47 +0000

    deleted flutter app

--- quickbite/android/app/src/main/kotlin/dev/kimikevin/quickbite/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dev.kimikevin.quickbite
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- quickbite/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'app/app.dart';
-
-void main() {
-
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const App());
-}
