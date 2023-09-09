8455f4be762759ae0f8fbb6fb04ad42a0919642b
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-09-09 13:48:30 +0000

    commented the old one

--- YouTubeDownloader/src/main/java/org/example/Main.java
+++ YouTubeDownloader/src/main/java/org/example/Main.java
@@ -4,25 +4,25 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        String videoUrl = "https://youtu.be/9i1gQ7w2V24";
-        String outputDirectory = "/Users/kelvineduful/Downloads/youtube";
-
-        try {
-            // Create a YouTube downloader instance
-            YoutubeDL youtubeDL = new YoutubeDl();
-
-            // Set the download options
-            DownloadOption downloadOption = new DownloadOptionBuilder()
-                    .setOutputDirectory(outputDirectory)
-                    .build();
-
-            // Download the video
-            youtubeDL.download(videoUrl, downloadOption);
-
-            // Print a success message
-            System.out.println("Video downloaded successfully!");
-        } catch (IOException | InterruptedException | YoutubeDLException e) {
-            e.printStackTrace();
-        }
+//        String videoUrl = "https://youtu.be/9i1gQ7w2V24";
+//        String outputDirectory = "/Users/kelvineduful/Downloads/youtube";
+//
+//        try {
+//            // Create a YouTube downloader instance
+//            YoutubeDL youtubeDL = new YoutubeDl();
+//
+//            // Set the download options
+//            DownloadOption downloadOption = new DownloadOptionBuilder()
+//                    .setOutputDirectory(outputDirectory)
+//                    .build();
+//
+//            // Download the video
+//            youtubeDL.download(videoUrl, downloadOption);
+//
+//            // Print a success message
+//            System.out.println("Video downloaded successfully!");
+//        } catch (IOException | InterruptedException | YoutubeDLException e) {
+//            e.printStackTrace();
+//        }
     }
 }
