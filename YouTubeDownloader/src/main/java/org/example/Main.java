03bdc08ddc5eb7d6fcf93e0c5c143f1b65e71ca2
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-09-09 14:39:39 +0000

    comment

--- YouTubeDownloader/src/main/java/org/example/Main.java
+++ YouTubeDownloader/src/main/java/org/example/Main.java
@@ -8,41 +8,41 @@ import java.net.URL;
 
 public class Main {
     public static void main(String[] args) {
-        downloadDL();
-        downloadVGet();
+//        downloadDL();
+//        downloadVGet();
     }
 
-    public static void downloadVGet() {
-        try {
-            String url = "https://www.youtube.com/watch?v=8eMCJSbmI3g";
-            String path = "/Users/kelvineduful/Downloads/youtube";
-            VGet v = new VGet(new URL(url), new File(path));
-            v.download();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static void downloadDL() {
-        String videoUrl = "https://youtu.be/9i1gQ7w2V24";
-        String outputDirectory = "/Users/kelvineduful/Downloads/youtube";
-
-        try {
-            // Create a YouTube downloader instance
-            YoutubeDL youtubeDL = new YoutubeDL();
-
-            // Set the download options
-            DownloadOption downloadOption = new DownloadOptionBuilder()
-                    .setOutputDirectory(outputDirectory)
-                    .build();
-
-            // Download the video
-            youtubeDL.download(videoUrl, downloadOption);
-
-            // Print a success message
-            System.out.println("Video downloaded successfully!");
-        } catch (IOException | InterruptedException | YoutubeDLException e) {
-            e.printStackTrace();
-        }
-    }
+//    public static void downloadVGet() {
+//        try {
+//            String url = "https://www.youtube.com/watch?v=8eMCJSbmI3g";
+//            String path = "/Users/kelvineduful/Downloads/youtube";
+//            VGet v = new VGet(new URL(url), new File(path));
+//            v.download();
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//    public static void downloadDL() {
+//        String videoUrl = "https://youtu.be/9i1gQ7w2V24";
+//        String outputDirectory = "/Users/kelvineduful/Downloads/youtube";
+//
+//        try {
+//            // Create a YouTube downloader instance
+//            YoutubeDL youtubeDL = new YoutubeDL();
+//
+//            // Set the download options
+//            DownloadOption downloadOption = new DownloadOptionBuilder()
+//                    .setOutputDirectory(outputDirectory)
+//                    .build();
+//
+//            // Download the video
+//            youtubeDL.download(videoUrl, downloadOption);
+//
+//            // Print a success message
+//            System.out.println("Video downloaded successfully!");
+//        } catch (IOException | InterruptedException | YoutubeDLException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
