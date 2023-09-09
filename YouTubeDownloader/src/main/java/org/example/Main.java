f8836f94266002864941212d24b35997aeb30384
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-09-09 14:13:44 +0000

    main

--- YouTubeDownloader/src/main/java/org/example/Main.java
+++ YouTubeDownloader/src/main/java/org/example/Main.java
@@ -8,26 +8,7 @@ import java.net.URL;
 
 public class Main {
     public static void main(String[] args) {
-//        String videoUrl = "https://youtu.be/9i1gQ7w2V24";
-//        String outputDirectory = "/Users/kelvineduful/Downloads/youtube";
-//
-//        try {
-//            // Create a YouTube downloader instance
-//            YoutubeDL youtubeDL = new YoutubeDl();
-//
-//            // Set the download options
-//            DownloadOption downloadOption = new DownloadOptionBuilder()
-//                    .setOutputDirectory(outputDirectory)
-//                    .build();
-//
-//            // Download the video
-//            youtubeDL.download(videoUrl, downloadOption);
-//
-//            // Print a success message
-//            System.out.println("Video downloaded successfully!");
-//        } catch (IOException | InterruptedException | YoutubeDLException e) {
-//            e.printStackTrace();
-//        }
+        downloadDL();
         downloadVGet();
     }
 
@@ -39,6 +20,29 @@ public class Main {
             v.download();
         } catch (Exception e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public static void downloadDL() {
+        String videoUrl = "https://youtu.be/9i1gQ7w2V24";
+        String outputDirectory = "/Users/kelvineduful/Downloads/youtube";
+
+        try {
+            // Create a YouTube downloader instance
+            YoutubeDL youtubeDL = new YoutubeDL();
+
+            // Set the download options
+            DownloadOption downloadOption = new DownloadOptionBuilder()
+                    .setOutputDirectory(outputDirectory)
+                    .build();
+
+            // Download the video
+            youtubeDL.download(videoUrl, downloadOption);
+
+            // Print a success message
+            System.out.println("Video downloaded successfully!");
+        } catch (IOException | InterruptedException | YoutubeDLException e) {
+            e.printStackTrace();
         }
     }
 }
